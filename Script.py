a9012b2d849ec8305bd75e9faef15c887e578589
theotherhades <64885838+theotherhades@users.noreply.github.com>
2022-06-10 17:00:01 +1200

    Fix to jittery movement, minor touchups

--- Script.py
+++ Script.py
@@ -1,29 +1,37 @@
-#imports pygame & time
+# imports pygame & time
 from multiprocessing.connection import wait
-import pygame,time
+
+import os
+
+# Had some problems installing pygame so I added this as a safety catch lol
+try:
+    import pygame
+except ModuleNotFoundError:
+    os.system('pip install pygame')
+
 pygame.init()
 
-#character
-char = pygame.image.load(r'C:\Users\eddyg\OneDrive\Bureau\Projects\SirGreyAlot\images\Character.png')
-pace = 0.1
+# character
+char = pygame.image.load(r'SirGreyAlot-Code\Character.png')
+pace = 1
 posX = 0
 posY = 0
 
-#sets window settings
+# sets window settings
 size = width, height = 1920, 1080
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Project SirGreyAlot")
 bgrndclr = (50,168,66)
 
 
-#game loop
+# game loop
 running = True
 
 while running:
     screen.fill(bgrndclr)
     screen.blit(char,(posX,posY))
 
-    #MOVEMENT
+    # MOVEMENT
     keyPressed = pygame.key.get_pressed()
     if keyPressed[pygame.K_w]:
         posY -= pace
@@ -37,12 +45,12 @@ while running:
     if keyPressed[pygame.K_d]:
         posX += pace 
 
-    #for loop through the event queue
+    # for loop through the event queue
     for event in pygame.event.get():        
-        #check for QUIT event
+        # check for QUIT event
         if event.type == pygame.QUIT:
             running = False
 
-        pygame.display.update() 
+    pygame.display.update() 
 
 pygame.quit()
